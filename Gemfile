73e505225053202b9451e2e3d1a4a2260b3d88af
Ronald Tse <ronald.tse@ribose.com>
2025-04-28 16:55:22 +0800

    feat: clean up code

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'rake', '~> 13.0'
 gem "rspec"
+
+gemspec
--- lib/hrma/commands/config.rb
+++ lib/hrma/commands/config.rb
@@ -7,8 +7,13 @@ require_relative "../config"
 
 module Hrma
   module Commands
+    # Thor command for managing configuration
     class Config < Thor
       desc "get KEY", "Get a configuration value"
+      # Get a configuration value
+      #
+      # @param key [String] Configuration key to get
+      # @return [void]
       def get(key)
         case key
         when "cache_dir"
@@ -22,6 +27,11 @@ module Hrma
       end
 
       desc "set KEY VALUE", "Set a configuration value"
+      # Set a configuration value
+      #
+      # @param key [String] Configuration key to set
+      # @param value [String] Value to set
+      # @return [void]
       def set(key, value)
         config_file = File.expand_path("~/.hrma/config.yml")
         config = File.exist?(config_file) ? YAML.load_file(config_file) : {}
@@ -43,6 +53,11 @@ module Hrma
 
       private
 
+      # Save configuration to file
+      #
+      # @param config [Hash] Configuration hash
+      # @param config_file [String] Path to configuration file
+      # @return [void]
       def save_config(config, config_file)
         FileUtils.mkdir_p(File.dirname(config_file))
         File.write(config_file, config.to_yaml)
